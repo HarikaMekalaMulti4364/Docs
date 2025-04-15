b7e51d4c13a783d4ca58f34117862256ef66da13
HarikaMekalaMulti4364 <harika.mekala@multicorewareinc.com>
2025-04-15 14:36:53 +0530

    Update Fill_conv.py

--- Fill_conv.py
+++ Fill_conv.py
@@ -54,30 +54,34 @@ r/constant_of_shape_base.h:68 onnxruntime::ConstantOfShapeBase<EnabledOutputType
 le, signed char, short int, int, unsigned char, short unsigned int, unsigned int, long unsigned int, bool, onnxruntime::BFloat16>] t_proto_p->dims()[0] == 1 was false. Must have a single dimension of 1 
 
 
+import numpy as np
+from onnx import helper, TensorProto
+
 @Converter.Register("FILL")
 def parse_FILL(parser):
-    shape_name = parser.get_tensor_name(parser.inputs[0])
-    output_name = parser.get_tensor_name(parser.outputs[0])
-
+    shape_name = parser.get_tensor_name(parser.inputs[0])  # shape input
+    output_name = parser.get_tensor_name(parser.outputs[0])  # output
     ip_quant_params = parser._get_input_quantization_params()
     op_quant_params = parser._get_output_quantization_params()
 
-    # Get constant value (must be convertible to numpy)
-    fill_value = parser.get_constant_node(parser.inputs[1])  # scalar value
-    fill_tensor = np.array([fill_value], dtype=np.float32)  # shape=[1] required
+    # Get the constant scalar value
+    fill_value = parser.get_constant_node(parser.inputs[1])
+    
+    # Must wrap into a tensor of shape [1]
+    value_tensor = np.array([fill_value], dtype=np.float32)
 
-    fill_attr = helper.make_tensor(
-        name="fill_value",
+    value_attr = helper.make_tensor(
+        name="value",
         data_type=TensorProto.FLOAT,
-        dims=[1],
-        vals=fill_tensor
+        dims=[1],  # Must be [1]
+        vals=value_tensor
     )
 
     parser.add_onnx_operator(
         "ConstantOfShape",
         [shape_name],
         [output_name],
-        {"value": fill_attr},
+        {"value": value_attr},
         ip_quant_params,
         op_quant_params
     )
