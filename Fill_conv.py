45a8ef927087ae7a9580a512f14e5a117b156692
HarikaMekalaMulti4364 <harika.mekala@multicorewareinc.com>
2025-04-15 14:29:58 +0530

    Update Fill_conv.py

--- Fill_conv.py
+++ Fill_conv.py
@@ -52,3 +52,32 @@ def parse_FILL(parser):
  onnxruntime.capi.onnxruntime_pybind11_state.RuntimeException: [ONNXRuntimeError] : 6 : RUNTIME_EXCEPTION : Exception during initialization: /localdev/lucia/build_onnxruntime_1.19/onnxruntime/onnxruntime/core/providers/cpu/generato
 r/constant_of_shape_base.h:68 onnxruntime::ConstantOfShapeBase<EnabledOutputTypeList>::ConstantOfShapeBase(const onnxruntime::OpKernelInfo&) [with EnabledOutputTypeList = onnxruntime::TypeList<long int, onnxruntime::MLFloat16, float, doub
 le, signed char, short int, int, unsigned char, short unsigned int, unsigned int, long unsigned int, bool, onnxruntime::BFloat16>] t_proto_p->dims()[0] == 1 was false. Must have a single dimension of 1 
+
+
+@Converter.Register("FILL")
+def parse_FILL(parser):
+    shape_name = parser.get_tensor_name(parser.inputs[0])
+    output_name = parser.get_tensor_name(parser.outputs[0])
+
+    ip_quant_params = parser._get_input_quantization_params()
+    op_quant_params = parser._get_output_quantization_params()
+
+    # Get constant value (must be convertible to numpy)
+    fill_value = parser.get_constant_node(parser.inputs[1])  # scalar value
+    fill_tensor = np.array([fill_value], dtype=np.float32)  # shape=[1] required
+
+    fill_attr = helper.make_tensor(
+        name="fill_value",
+        data_type=TensorProto.FLOAT,
+        dims=[1],
+        vals=fill_tensor
+    )
+
+    parser.add_onnx_operator(
+        "ConstantOfShape",
+        [shape_name],
+        [output_name],
+        {"value": fill_attr},
+        ip_quant_params,
+        op_quant_params
+    )
