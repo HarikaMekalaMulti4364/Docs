33f72d66bf977af4cc8496a2fd409146dcc410e6
HarikaMekalaMulti4364 <harika.mekala@multicorewareinc.com>
2025-04-16 14:10:21 +0530

    Update floor_div.py

--- floor_div.py
+++ floor_div.py
@@ -17,3 +17,22 @@ def parse_floor_div(parser):
     
     # Insert the Floor operation
     parser.add_onnx_operator("Floor", [cast_output_name], [output_name])
+
+
+
+@Converter.Register("FLOOR_DIV")
+def parse_FLOOR_DIV(parser):
+    input_names = parser.write_inputs_to_onnx_net(broadcast=True)
+    # not considered quantized MUL yet
+    # output_name should also support quant
+    output_name = parser.get_tensor_name(parser.outputs[0])
+    intermediate_output_name = "Div_output" + output_name
+    parser.add_onnx_operator("Div", input_names, [intermediate_output_name])
+    shape_name = parser.get_tensor_name(intermediate_output_name)
+    if parser.dtype_to_numpy_type[shape_name.Type()] == np.int32:
+        cast_name = shape_name + "/cast_to_float32"
+        parser.add_onnx_operator(
+            "Cast", [shape_name], [cast_name], attr_dict={"to": TensorProto.FLOAT}
+        )
+        shape_name = cast_name
+    parser.add_onnx_operator("Floor", [shape_name], [output_name])
