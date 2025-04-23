7a6808676960031f30e015d1b0d5ea94c84c0a15
HarikaMekalaMulti4364 <harika.mekala@multicorewareinc.com>
2025-04-23 12:48:09 +0530

    Update BSD.py

--- BSD.py
+++ BSD.py
@@ -1,3 +1,34 @@
+
+You are in 'detached HEAD' state. You can look around, make experimental
+changes and commit them, and you can discard any commits you make in this
+state without impacting any branches by switching back to a branch.
+
+If you want to create a new branch to retain commits you create, you may
+do so (now or later) by using -c with the switch command. Example:
+
+  git switch -c <new-branch-name>
+
+Or undo this operation with:
+
+  git switch -
+
+Turn off this advice by setting config variable advice.detachedHead to false
+
+HEAD is now at 3c5d9f10 Enable python_api compile tests
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 import numpy as np
 import onnxruntime as ort
